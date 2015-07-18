2e54764a9665a2755acf8f8ccdbd427b451b11b3
Lucas Garron <code@garron.net>
2015-07-18 02:57:38 -0300

    Almost working.

--- f2lfast.cpp
+++ f2lfast.cpp
@@ -30,8 +30,8 @@ int main(int argc, char *argv[]) {
 
   // cubepos pos = alg2pos("R U' R' U R2 U2 R' L' B' R B L U2 R2");
   // cubepos scramble = alg2pos("F' R' U2 R F");
-  cubepos scramble = alg2pos("U' R U R' U L' U' L");
-  // cubepos scramble = alg2pos("R U' R' U R U2 R' U' R U R' L' U' L");
+  // cubepos scramble = alg2pos("U' R U R' U L' U' L");
+  cubepos scramble = alg2pos("R U' R' U R U2 R' U' R U R' L' U' L");
   string solution = solve(scramble);
   cout << "Solution: " << solution << endl;
 }
--- solve.cpp
+++ solve.cpp
@@ -4,7 +4,7 @@ string indentation[4] = {"    ", "   ", "  ", " "};
 // The i-th position from the right is set iff slot #i is solved.
 typedef unsigned char F2LSlotMask;
 
-F2LSlotMask slotMask(cubepos& pos) {
+F2LSlotMask slotMask(const cubepos& pos) {
   F2LSlotMask mask = 0;
   for (size_t slot = 0; slot < NUM_SLOTS; slot++) {
     if (isSlotSolved(F2LSlot(slot), pos)) {
@@ -22,8 +22,10 @@ bool firstMaskIsSubsetOfSecond(F2LSlotMask first, F2LSlotMask second) {
   return (second & (~first)) != 0;
 }
 
+const int MAX_DEPTH_PER_SLOT = 3;
+
 // Returns null if no solution is found.
-string solveF2LWithSkip(const cubepos& scramble, int depthRemaining) {
+string solveF2LWithSkip(const cubepos& scramble, int depthRemaining, F2LSlotMask mask) {
   if (isSolvedUpToAUF(scramble)) {
     return "!";
   }
@@ -34,12 +36,21 @@ string solveF2LWithSkip(const cubepos& scramble, int depthRemaining) {
     cubepos pos1;
     cubepos::mul(scramble, auf.pos, pos1);
     for (int slot = 0; slot < NUM_SLOTS; slot++) {
+      if (slotInMaskIsSolved(F2LSlot(slot), mask)) {
+        continue;
+      }
       for (Alg trigger : triggers[slot]) {
         cubepos pos2;
         cubepos::mul(pos1, trigger.pos, pos2);
 
-        // cout << indentation[depthRemaining] << auf.name_ + string(" ") + trigger.name_  << " (" << depthRemaining << ")" << endl;
-        string solution = solveF2LWithSkip(pos2, depthRemaining - 1);
+        F2LSlotMask newMask = slotMask(pos2);
+        int nextDepth = depthRemaining - 1;
+        if (firstMaskIsSubsetOfSecond(mask, newMask)) {
+          nextDepth = MAX_DEPTH_PER_SLOT;
+          cout << indentation[depthRemaining] << auf.name_ + string(" ") + trigger.name_  << " (" << depthRemaining << ")" << endl;
+        }
+
+        string solution = solveF2LWithSkip(pos2, depthRemaining - 1, newMask);
         if (solution.length() > 0) {
           return auf.name_ + string(" ") + trigger.name_ + string(" / ") + solution;
         }
@@ -51,5 +62,12 @@ string solveF2LWithSkip(const cubepos& scramble, int depthRemaining) {
 
 string solve(const cubepos& scramble) {
   // Assume cross is solved for now.
-  return solveF2LWithSkip(scramble, 2);
+  string solution;
+  solution = solveF2LWithSkip(scramble, 1, slotMask(scramble));
+  if (solution.length() > 0) { return solution; }
+  solution = solveF2LWithSkip(scramble, 2, slotMask(scramble));
+  if (solution.length() > 0) { return solution; }
+  solution = solveF2LWithSkip(scramble, 3, slotMask(scramble));
+  if (solution.length() > 0) { return solution; }
+  return solution;
 }
